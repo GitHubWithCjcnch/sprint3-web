7debaf25f6ba011115cb86bf2444c72b3d183392
caio vitor <githubwithcjcnch@gmail.com>
2023-09-12 21:44:32 -0300

    instalando react router para o projeto e setando primeiras as páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react'
+import Home from './routes/Home.jsx'
+import Erro404 from './routes/Erro404.jsx'
+import Projeto from './routes/Projeto.jsx'
+import About from './routes/AboutUs.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Erro404/>,
+    children:[
+      {
+        path: "/",
+        element:<Home/>
+      },
+      {
+        path: "/projeto",
+        element:<Projeto/>
+      },
+      {
+        path: "/sobre",
+        element:<About/>
+      }
+    ]
+  }
+  ])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+)
 
 export default App
