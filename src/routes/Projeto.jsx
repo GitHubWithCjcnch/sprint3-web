312de89f453ca1c49dacc2de12ceecc06772b588
Emii2003 <emile.maia@etec.sp.gov.br>
2023-09-12 22:45:15 -0300

    Atualizações nas rotas

--- src/routes/Projeto.jsx
+++ src/routes/Projeto.jsx
@@ -9,17 +9,17 @@ export default function Projeto(){
             <h1>Entenda o processo</h1>
             <div className="row_card">
                 <div className="card">
-                    <img src={imgCard1} alt="Imagem de uma fazenda"/>
-                    <p>O desperdício na agricultura ocorre devido a vários fatores, como perdas na colheita, armazenamento inadequado, falta de infraestrutura e demanda insuficiente, resultando em descarte de alimentos perfeitamente comestíveis.</p>
+                    <img src={imgCard1} alt="Logo da Porto Seguro"/>
+                    <p>Nossa tarefa é facilitar o seu contato com a Porto Seguro, para isso, é preciso que insira seu CPF no login caso seja um assegurado, caso contrário, você terá a opção de solicitar um guincho avulso apenas enviando dados como seu CPF, nome e localização, é fácil e atende a urgência do pedido de guincho.</p>
                 </div>
                 <div className="card">
-                <img src={imgCard2} alt="Imagem de uma fazenda"/>
-                    <p>Durante o transporte até o mercado, ocorre um significativo desperdício de alimentos. Fatores como manuseio inadequado, falta de refrigeração adequada e longas distâncias podem levar à perda de produtos perecíveis. </p>
+                <img src={imgCard2} alt="Imagem de um celular"/>
+                    <p>Para nos solicitar, basta fazer login e já poderá acessar nossa plataforma, insira todos apenas a placa e o peso que seu veículo carrega, terá a opção de enviar foto se necessário. Nosso assistente virtual irá retornar o tipo de guincho apropriado para sua situação.</p>
                 </div>
 
                 <div className="card">
-                <img src={imgCard3} alt="Imagem de uma fazenda"/>
-                    <p>A implementação de práticas logísticas eficientes e a adoção de tecnologias, como a Internet das Coisas, podem ajudar a reduzir as perdas e garantir que os alimentos cheguem em bom estado ao mercado.</p> 
+                <img src={imgCard3} alt="Imagem de um veículo sendo guinchado"/>
+                    <p>Este processo de solicitação economiza tempo de espera, não é necessário que faça atendimento direto com a Porto, garantimos qualidade e assertividade. Através do nosso sistema, é possível acompanhar sua solicitação de guincho em tempo real, podendo cancelar quando quiser. </p> 
                 </div>
             </div>
         </div>
