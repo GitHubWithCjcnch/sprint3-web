89526b0403ba190687ae433089d4b073e4b6a938
eduardo <eduardo.guedesdzn@gmail.com>
2023-09-12 22:23:05 -0300

    corrigindo o App.jsx e adicionando o router ao main

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import React from 'react'
-import Home from './routes/Home.jsx'
-import Erro404 from './routes/Erro404.jsx'
-import Projeto from './routes/Projeto.jsx'
-import About from './routes/AboutUs.jsx'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import Menu from './componentes/Menu'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <Erro404/>,
-    children:[
-      {
-        path: "/",
-        element:<Home/>
-      },
-      {
-        path: "/projeto",
-        element:<Projeto/>
-      },
-      {
-        path: "/sobre",
-        element:<About/>
-      }
-    ]
-  }
-  ])
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>
-)
+function App() {
+
+  return (
+    <>
+      <Menu/>
+      <Outlet/>
+    </>
+  )
+}
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import React from 'react'
+import Home from './routes/Home.jsx'
+import Erro404 from './routes/Erro404.jsx'
+import Projeto from './routes/Projeto.jsx'
+import About from './routes/AboutUs.jsx'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Erro404/>,
+    children:[
+      {
+        path: "/",
+        element:<Home/>
+      },
+      {
+        path: "/projeto",
+        element:<Projeto/>
+      },
+      {
+        path: "/sobre",
+        element:<About/>
+      }
+    ]
+
+  }
+  ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
 )
+
+export default App
